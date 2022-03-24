54a2b9c265171ae6178358171b317ce5dbf3b32a
thewerther <johannes.puntigam@student.tugraz.at>
2022-03-24 20:12:43 +0100

    Add the werther id67 keymap

---
+++ keyboards/id67/keymaps/via/config.h
@@ -0,0 +1,14 @@
+/* Copyright 2021 Tybera
+ * Copyright 2022 thewerther
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#if defined(RGB_MATRIX_ENABLE)
+    #define VIA_QMK_RGBLIGHT_ENABLE
+#endif
